a70332b6a48d85143778c99ad7a49955bc45d73c
Junaid Khan <junaidsultan92pk@gmail.com>
2025-03-18 11:37:53 +0500

    updated

--- routers/profile.js
+++ routers/profile.js
@@ -4,8 +4,9 @@ const jwt = require('jsonwebtoken');
 const UserDb = require('../database/models/UserDb'); // Importing the User database model for user data handling
 const authenticateToken = require('../middlewares/authMiddleware')
 
-router.get('/' , authenticateToken ,(req , res)=>{
-       res.send("Login Succesfull")
-})
+router.get('/', authenticateToken, (req, res) => {
+       res.json({ message: "Login Successful", user: req.user });
+});
+
 
 module.exports = router;
